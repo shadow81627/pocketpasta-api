783a9377a2a59f44554e0e738a95ac643325384c
Damien Robinson <damien.robinson@pocketpasta.com>
2021-03-20 22:59:30 +1000

    test(user): add seeder

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -4,15 +4,15 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+class UsersTableSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      *
      * @return void
      */
     public function run()
     {
-        $this->call(UsersTableSeeder::class);
+        \App\Models\User::factory(10)->create();
     }
 }
