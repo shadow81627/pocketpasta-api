79057cc0a55cd7341f9a2fc6f572b50386d86024
Damien Robinson <damien.robinson@pocketpasta.com>
2021-03-21 14:18:39 +1000

    chore: force https for production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,6 +24,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        if($this->app->environment('production')) {
+            \Illuminate\Support\Facades\URL::forceScheme('https');
+        }
         JsonResource::withoutWrapping();
     }
 }
