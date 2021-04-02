2732f3531e71dbe9d39c689e2d8dfc402663d365
Damien Robinson <damien.robinson@pocketpasta.com>
2021-04-02 21:58:11 +1000

    style(thing controller): lint fix

--- app/Http/Controllers/Api/V1/ThingController.php
+++ app/Http/Controllers/Api/V1/ThingController.php
@@ -5,13 +5,11 @@ namespace App\Http\Controllers\Api\V1;
 use App\Http\Controllers\Controller;
 use LaravelJsonApi\Core\Responses\DataResponse;
 use LaravelJsonApi\Laravel\Http\Controllers\Actions;
-
 use App\Models\Thing;
 use App\JsonApi\V1\Things\ThingCollectionQuery;
 
 class ThingController extends Controller
 {
-
     use Actions\FetchMany;
     use Actions\FetchOne;
     use Actions\Store;
@@ -27,5 +25,4 @@ class ThingController extends Controller
     {
         return new DataResponse(Thing::search($request->search)->get());
     }
-
 }
