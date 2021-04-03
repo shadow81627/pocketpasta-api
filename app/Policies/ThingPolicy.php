516d55d5da4025b803b4047db32b22878758b9ad
Damien Robinson <damien.robinson@pocketpasta.com>
2021-04-03 11:12:37 +1000

    fix(thing): allow create, update and delete

--- app/Policies/ThingPolicy.php
+++ app/Policies/ThingPolicy.php
@@ -41,7 +41,7 @@ class ThingPolicy
      */
     public function create(User $user)
     {
-        //
+        return true;
     }
 
     /**
@@ -53,7 +53,7 @@ class ThingPolicy
      */
     public function update(User $user, Thing $thing)
     {
-        //
+        return true;
     }
 
     /**
@@ -65,7 +65,7 @@ class ThingPolicy
      */
     public function delete(User $user, Thing $thing)
     {
-        //
+        return true;
     }
 
     /**
@@ -77,7 +77,7 @@ class ThingPolicy
      */
     public function restore(User $user, Thing $thing)
     {
-        //
+        return true;
     }
 
     /**
