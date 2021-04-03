f33b79e4fab7a0587a4a9f846653c50588cf4346
Damien Robinson <damien.robinson@pocketpasta.com>
2021-04-03 11:12:50 +1000

    fix(thing): description nullable

--- database/migrations/2021_03_20_131248_create_things_table.php
+++ database/migrations/2021_03_20_131248_create_things_table.php
@@ -16,7 +16,7 @@ class CreateThingsTable extends Migration
         Schema::create('things', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('description');
+            $table->string('description')->nullable();
             $table->timestamps();
             $table->softDeletes();
         });
