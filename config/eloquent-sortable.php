e9eb67093689a3bfee3addb25377f500511dd1c0
Damien Robinson <damien.robinson@pocketpasta.com>
2021-04-03 21:35:52 +1000

    chore(deps): add eloquent sortable

---
+++ config/eloquent-sortable.php
@@ -0,0 +1,13 @@
+<?php
+return [
+    /*
+     * The name of the column that will be used to sort models.
+     */
+    'order_column_name' => 'sort_order',
+
+    /*
+     * Define if the models should sort when creating. When true, the package
+     * will automatically assign the highest order number to a new model
+     */
+    'sort_when_creating' => true,
+  ];
