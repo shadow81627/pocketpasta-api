5b19021c4cc9ea97e37edddcc371e1795d538f6b
Damien Robinson <damien.robinson@pocketpasta.com>
2021-06-20 12:29:50 +1000

    fix(error): publish error pages

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,29 +1,29 @@
-const defaultTheme = require('tailwindcss/defaultTheme');
+const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
-    purge: [
-        './resources/**/*.antlers.html',
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './vendor/laravel/jetstream/**/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
-        './resources/js/**/*.vue',
-        './content/**/*.md',
-    ],
+  purge: [
+    "./resources/**/*.antlers.html",
+    "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
+    "./vendor/laravel/jetstream/**/*.blade.php",
+    "./storage/framework/views/*.php",
+    "./resources/views/**/*.blade.php",
+    "./resources/js/**/*.vue",
+    "./content/**/*.md",
+  ],
 
-    theme: {
-        extend: {
-            fontFamily: {
-                sans: ['Nunito', ...defaultTheme.fontFamily.sans],
-            },
-        },
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: [...defaultTheme.fontFamily.sans],
+      },
     },
+  },
 
-    variants: {
-        extend: {
-            opacity: ['disabled'],
-        },
+  variants: {
+    extend: {
+      opacity: ["disabled"],
     },
+  },
 
-    plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography')],
+  plugins: [require("@tailwindcss/forms"), require("@tailwindcss/typography")],
 };
