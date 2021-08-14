170801c3c8b8bc7820d7faf7383c960caaaee710
Damien Robinson <damien.robinson@pocketpasta.com>
2021-08-14 10:33:52 +1000

    fix(migrations): add macro service provider

--- app/Providers/MacroServiceProvider.php
+++ app/Providers/MacroServiceProvider.php
@@ -24,7 +24,6 @@ class MacroServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-
         Blueprint::macro('thingFields', function () {
             /** @var Blueprint $this */
             $this->id();
